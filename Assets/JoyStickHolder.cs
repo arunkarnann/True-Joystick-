8c02ef95f51b71997a43d6d0dd3d5fdee6ea0a9f
ArunK <arunkarnaa@gmail.com>
2016-06-15 17:55:54 +0530

    First Commit
    Project first commit

---
+++ Assets/JoyStickHolder.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+public class JoyStickHolder : MonoBehaviour,IPointerDownHandler, IPointerUpHandler {
+
+
+	public Transform Joystick;
+
+	public void OnPointerDown(PointerEventData data){
+		print(data.delta.x+""+data.delta.y.ToString());
+		print(data.position.ToString());
+		Joystick.position =  new Vector3(data.position.x,data.position.y,0);
+	}
+	public void OnPointerUp(PointerEventData data){
+		Joystick.position =  new Vector3(Input.mousePosition.x,Input.mousePosition.y,0);
+	}
+
+}
